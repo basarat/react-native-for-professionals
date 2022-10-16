de915c8ef75a8a4f982a359e0d26b67cc9fed210
Basarat <basaratali@gmail.com>
2022-10-17 09:21:18 +1100

    better

--- DemoReactNative/src/App.tsx
+++ DemoReactNative/src/App.tsx
@@ -1,47 +1,17 @@
 import React, { useRef } from 'react';
-import { FlatList, Button, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View, ViewStyle } from 'react-native';
-import { humanize } from './utils/humanize';
+import { FlatList, Button, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View, ViewStyle, SectionList } from 'react-native';
+import { sections } from './utils/sectionsData';
 
 const styles = StyleSheet.create({
-  fill: { flex: 1 }
+
 });
 
-const items: string[] = [...Array(100_000)].map((_, i) => humanize(i + 1));
-
-const ITEM_HEIGHT = 16;
 export default function App() {
-  const ref = useRef<FlatList>(null);
   return (
-    <SafeAreaView style={styles.fill}>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar hidden />
-      <Button title="Scroll To End" onPress={() => { 
-        ref.current?.scrollToEnd();
-      }} />
-      <Button title="Scroll Halfway" onPress={() => {
-        ref.current?.scrollToIndex({
-          index: items.length / 2,
-        });
-      }} />
 
-      <FlatList
-        ref={ref}
-        data={items}
-        initialNumToRender={100}
-        maxToRenderPerBatch={100}
-        keyExtractor={(item, index) => item}
-        getItemLayout={(data, index) => {
-          return { index, length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index };
-        }}
-        renderItem={({ item, index }) => {
-          return (
-            <View style={{ height: ITEM_HEIGHT }}>
-              <Text style={{ fontSize: ITEM_HEIGHT, textAlign: 'center' }}>
-                {item}
-              </Text>
-            </View>
-          );
-        }}
-      />
+      
     </SafeAreaView>
   );
 }
