a39a0c4df74a005dc28eb8e80b1772204a905607
Basarat <basaratali@gmail.com>
2023-05-07 13:03:49 +1000

    cleaner

--- DemoReactNative/.eslintrc.js
+++ DemoReactNative/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    // disable prettier
+    'prettier/prettier': false,
+    // allow inline styles
+    'react-native/no-inline-styles': false,
+    // eol not required
+    'eol-last': false,
+  },
 };
